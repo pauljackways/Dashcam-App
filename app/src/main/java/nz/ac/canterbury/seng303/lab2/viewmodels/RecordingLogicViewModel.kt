575fc9ffa1f275afd3910eb4a70900bb199f6ddc
FindlayRoyds <findlayroyds@gmail.com>
2024-10-02 14:40:58 +1300

    Implement functionality for recording logic view model to run a runnable at a given frequency

--- app/src/main/java/nz/ac/canterbury/seng303/lab2/viewmodels/RecordingLogicViewModel.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/lab2/viewmodels/RecordingLogicViewModel.kt
@@ -1,19 +1,68 @@
 package nz.ac.canterbury.seng303.lab2.viewmodels
 
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.ViewModel
 
 class RecordingLogicViewModel : ViewModel() {
-    val isRecording = false
+    var isRecording = false
+        private set
 
-    fun startRecording() {
 
+//    TODO private var camera: Camera class
+    private var videoSectionLengthMillis: Long? = null
+    private var videoSectionSaveLocation: String? = null
+
+    private val taskHandler = Handler(Looper.getMainLooper())
+    private val startNextVideoSection = object : Runnable {
+        override fun run() {
+            val lengthMillisNullSafe = videoSectionLengthMillis
+                ?: throw IllegalArgumentException("Video section length is null!")
+            val saveLocationNullSafe = videoSectionSaveLocation
+                ?: throw IllegalArgumentException("Save location is null!")
+
+            println("RUNNING!!!") // for testing
+
+            // TODO Delete old video section (e.g., from 30 seconds ago) if it exists?
+            stopAndSaveVideoSection(saveLocationNullSafe)
+            startVideoSection()
+
+            taskHandler.postDelayed(this, lengthMillisNullSafe)
+
+        }
     }
 
-    fun stopAndSaveRecording() {
 
+    fun startRecording(timePeriodMillis: Long, saveLocation: String) {
+        isRecording = true
+        videoSectionLengthMillis = timePeriodMillis
+        videoSectionSaveLocation = saveLocation
+
+        taskHandler.post(startNextVideoSection)
+    }
+
+    fun stopAndSaveRecording(saveLocation: String /* might not be string */) {
+        // Stop task from running periodically
+        taskHandler.removeCallbacks(startNextVideoSection)
+        stopAndSaveVideoSection(saveLocation)
+
+        // TODO stitch together videos and save to gallery (ffmpeg)
     }
 
     fun cancelRecording() {
+        // Stop task from running periodically
+        taskHandler.removeCallbacks(startNextVideoSection)
+        // TODO camera.stopRecording()
+        // TODO clear saved video sections?
+    }
 
+
+    private fun startVideoSection() {
+        // camera.startRecording()
+    }
+
+    private fun stopAndSaveVideoSection(saveLocation: String /* might not be string */) {
+        // TODO save to save location with date / time identifier
+        // TODO camera.stopRecording(saveLocation, current time)
     }
 }
