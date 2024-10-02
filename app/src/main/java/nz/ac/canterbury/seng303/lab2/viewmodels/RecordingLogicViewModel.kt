603eda145d88269b897a15f223148858679e7335
FindlayRoyds <findlayroyds@gmail.com>
2024-10-02 13:45:26 +1300

    Created skeleton for recording logic view model

---
+++ app/src/main/java/nz/ac/canterbury/seng303/lab2/viewmodels/RecordingLogicViewModel.kt
@@ -0,0 +1,19 @@
+package nz.ac.canterbury.seng303.lab2.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class RecordingLogicViewModel : ViewModel() {
+    val isRecording = false
+
+    fun startRecording() {
+
+    }
+
+    fun stopAndSaveRecording() {
+
+    }
+
+    fun cancelRecording() {
+
+    }
+}
