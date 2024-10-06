994879b95075e61f573fd2d347846a13f9b1b846
FindlayRoyds <findlayroyds@gmail.com>
2024-10-07 12:09:47 +1300

    Added driving notification to notification helper object

--- app/src/main/java/nz/ac/canterbury/seng303/lab2/util/Notification.kt
+++ app/src/main/java/nz/ac/canterbury/seng303/lab2/util/Notification.kt
@@ -17,7 +17,8 @@ import androidx.core.content.ContextCompat
 import nz.ac.canterbury.seng303.lab2.MainActivity
 
 object Notification {
-    const val RECORDING_NOTIFICATION_ID = 1
+    private const val RECORDING_NOTIFICATION_ID = 1
+    private const val DRIVING_NOTIFICATION_ID = 2
 
     private const val REQUEST_CODE_PERMISSIONS = 30369
 
@@ -95,5 +96,54 @@ object Notification {
     fun cancelRecordingNotification(context: Context) {
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.cancel(RECORDING_NOTIFICATION_ID)
+    }
+
+    fun sendDrivingNotification(context: Context) {
+        val channelId = "driving-detection-channel-id"
+        val notificationId = DRIVING_NOTIFICATION_ID
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        // TODO replace with string constant thingy
+        val name = "Driving Detected"
+        val descriptionText = "Notifications that display when the app detects you may be driving"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+        }
+
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(android.R.drawable.ic_menu_delete)
+            .setContentTitle("Are you driving?")
+            .setContentText("We detected you may be driving without the dashcam. Tap here to open the app")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+//            .setOngoing(true)
+        // TODO ongoing notifications requires a foreground service
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationId, builder.build())
+        }
+    }
+
+    fun cancelDrivingNotification(context: Context) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancel(DRIVING_NOTIFICATION_ID)
     }
 }
